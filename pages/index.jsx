48cbee148070e81ad353632f43ea9e68718b355a
Bianca Ribeiro Mazotti <biancaribeiromazotti@gmail.com>
2024-08-08 15:11:44 +0100

    Criação do Onboarding
    Criei o Onboarding como arquivo index e uma rota provisória para a home da aplicação

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,11 @@
 import styles from './index.module.css'
-import SidebarComponent from "../src/components/sidebar/sidebar"
-import HomeComponent from "../src/components/home/home"
-import HeaderComponent from '../src/components/header/header'
-
+import HeaderOnboardingComponent from '../src/components/headeronboarding/headeronboarding'
+import { MdMargin } from 'react-icons/md'
 
 export default function HomePage(){
     return(
-        <div className={styles.body}>
-                <div className={styles.header}>
-                    <HeaderComponent/>
-                </div>
-                <div className={styles.bottom}>
-                    <SidebarComponent className={styles.sidebar}/> 
-                    <HomeComponent className={styles.main}/>
-                </div>
+        <div className={styles.principal}>            
+                <HeaderOnboardingComponent/>
         </div>
     )
 }
