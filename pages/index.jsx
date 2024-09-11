9391e67ee362eec06aa0d1f643de173589bcd0e6
fabiodrneles <fabiodrneles@gmail.com>
2024-09-11 15:51:20 -0300

    criando footer para a home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,19 @@
 import styles from './index.module.css';
 import Header from '../src/components/header-home/header-home';
+import Footer from '../src/components/footer/footer';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 
 export default function HomePage() {
     return (
         <div className={styles.principal}>
             <Header/>
+            <h1>teste de tela</h1>
+
+            <Footer />
+
 
         </div>
     );
