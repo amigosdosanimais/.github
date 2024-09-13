91f7acbc17dfc59bd9e7914dbfe3135cfc1f7020
Bianca Ribeiro Mazotti <biancaribeiromazotti@gmail.com>
2024-09-13 10:54:57 -0300

    Ajustando dependências

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import styles from './index.module.css';
 import Header from '../src/components/header-home/header-home';
 import Footer from '../src/components/footer/footer';
-import { config } from '@fortawesome/fontawesome-svg-core';
+import {config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import WelcomeCard from '../src/components/welcomeCard/welcomeCard';
 import InfoCard from '../src/components/infoCard/infoCard';
