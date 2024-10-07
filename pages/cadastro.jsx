2b5149c2376da8308ddb8916c9c702c77c07c112
Bianca Ribeiro Mazotti <biancaribeiromazotti@gmail.com>
2024-10-07 11:46:18 -0300

    Update página da estrutura da página de cadastro

--- pages/cadastro.jsx
+++ pages/cadastro.jsx
@@ -3,6 +3,9 @@ import styles from './cadastro.module.css';
 import BreadcrumbsComponent from '../src/components/breadcrumbsComponent/breadcrumbsComponent';
 import Header from '../src/components/header-home/header-home';
 import Footer from '../src/components/footer/footer';
+import Divider from '../src/components/divider/divider'
+import InputComponent from '../src/components/input/input'
+import Button from '../src/components/button/button'
 
 export default function Cadastro () {
   
@@ -14,11 +17,41 @@ export default function Cadastro () {
         </div>
 
         <div className={styles.title}>
-          <h1>Teste de pagina</h1>
+          <h1>Cadastro</h1>
+          
         </div>
 
-        <Footer />
+        <div  className={styles.divider}>
+          <Divider ></Divider>
+        </div>
+        <div className={styles.texto}>
+          <p>Preencha os campos abaixo para fazer o cadastro</p>
+        </div>
+        <div className={styles.input}>
+          <InputComponent ></InputComponent>
+        </div>
+        <div className={styles.input}>
+          <InputComponent ></InputComponent>
+        </div>
+        <div className={styles.input}>
+          <InputComponent ></InputComponent>
+        </div>
+        <div className={styles.input}>
+          <InputComponent ></InputComponent>
+        </div>
+        <div className={styles.texto}><p>PASSWORD-CHECKLIST</p></div>
+        <div className={styles.input}>
+          <InputComponent ></InputComponent>
+        </div>
 
+        <div className={styles.texto}><p>Li e concordo com os <a href="#">Termos de Uso</a> e <a href="#">Política de Privacidade</a></p></div>
+
+        <div className={styles.button}><Button type='dark' text='Criar conta' width='526px' /></div>
+        <div className={styles.button}><Button type='light'text='Fazer login' width='526px'></Button></div>
+        <div className={styles.footer}>
+          <Footer />
+        </div>
+        
       </div>
     )
 }
